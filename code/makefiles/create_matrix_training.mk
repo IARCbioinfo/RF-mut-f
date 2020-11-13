f2336cfca9f836a460fc0698e25366ae3f51e713
adigenova <digenova@gmail.com>
2020-11-13 18:23:02 +0100

    setting genome and databases

--- code/makefiles/create_matrix_training.mk
+++ code/makefiles/create_matrix_training.mk
@@ -3,24 +3,14 @@
 #cromosomes considerated
 
 CHRS=chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY,chrM
-
-
+#directory with code and databases
+ROOT_D=/data/scratch/digenovaa/Somatic-reference-free/SNV-INDELs/RF-mut-f
 #we compute stats
-BCFTOOLS=/home/digenovaa/binaries/bcftools-1.10.2/BCFT/bin/bcftools
-TABIX=/home/digenovaa/miniconda3/bin/tabix
-vcf_stats=$(patsubst %.vcf.gz,%.stats,$(wildcard *.vcf.gz))
-%.stats:%.vcf.gz
-	${TABIX} $<
-	${BCFTOOLS} stats -r ${CHRS}  $< > $@
-stats:$(vcf_stats)
-#compute shared SNPs/INDELS with vars
-shared_stats=$(patsubst %.vcf.gz,%.shared.stats,$(wildcard *.vcf.gz))
-%.shared.stats:%.vcf.gz
-	 ${BCFTOOLS} stats -r ${CHRS} $< ../matched/$(subst .tonly.vcf.gz,.vcf.gz,$<) > $@
-shared : $(shared_stats)
+BCFTOOLS=bcftools
+TABIX=tabix
 
 #annotate gnomad
-GENOMAD_DB=/data/scratch/digenovaa/Somatic-reference-free/gnomad/gnomad.genome.vcf.bgz
+GENOMAD_DB=${ROOT_D}/databases/gnomad/gnomad.genome.vcf.bgz
 gnomad_annot_files=$(patsubst %.vcf.gz,%.gnomad.vcf.bgz,$(wildcard *.vcf.gz))
 %.gnomad.vcf.bgz:%.vcf.gz
 	${BCFTOOLS} annotate -r ${CHRS} -O z -o $@  -c "GNOMAD_AC:=AC,GNOMAD_AN:=AN" -m -NO_GNOMAD -a ${GENOMAD_DB} $<
@@ -28,7 +18,7 @@ gnomad_annot_files=$(patsubst %.vcf.gz,%.gnomad.vcf.bgz,$(wildcard *.vcf.gz))
 gnomad_annot:$(gnomad_annot_files)
 
 #annotate cosmic vars
-COSMIC_DB=/data/scratch/digenovaa/Somatic-reference-free/cosmic/CosmicSNV_INDELS.gnomad.vcf.bgz
+COSMIC_DB=${ROOT_DB}/cosmic/CosmicSNV_INDELS.gnomad.vcf.bgz
 cosmic_annot_files=$(patsubst %.gnomad.vcf.bgz,%.cosmic.vcf.bgz,$(gnomad_annot_files))
 %.cosmic.vcf.bgz:%.gnomad.vcf.bgz
 	${BCFTOOLS} annotate -r ${CHRS} -O z -o $@ -c ID -m +COSMIC -a ${COSMIC_DB} $<
@@ -38,38 +28,38 @@ comic_annot:$(cosmic_annot_files)
 #annotate cosmic genes and centromers
 add_centro_cgenes=$(patsubst %.cosmic.vcf.bgz,%.centro_cgenes.vcf.bgz,$(cosmic_annot_files))
 %.centro_cgenes.vcf.bgz:%.cosmic.vcf.bgz
-	${BCFTOOLS} annotate -a centromers.bed.gz -h centromers.hdr -c CNAME --columns CHROM,FROM,TO,CNAME -m +CENTROMER $< | \
-	${BCFTOOLS} annotate -a cosmic_gene_census.bed.gz -h cosmic_gene_census.hdr -c CGENE --columns CHROM,FROM,TO,CGENE -O z -o $@ -m +COSMIC_CENSUS_GENE -
-	tabix $@
+	${BCFTOOLS} annotate -a ${ROOT_D}/databases/misc/centromers.bed.gz -h ${ROOT_D}/databases/misc/centromers.hdr -c CNAME --columns CHROM,FROM,TO,CNAME -m +CENTROMER $< | \
+	${BCFTOOLS} annotate -a ${ROOT_D}/databases/misc/cosmic_gene_census.bed.gz -h ${ROOT_D}/databases/misc/cosmic_gene_census.hdr -c CGENE --columns CHROM,FROM,TO,CGENE -O z -o $@ -m +COSMIC_CENSUS_GENE -
+	${TABIX} $@
 centro_genes:$(add_centro_cgenes)
 
 #annotate variants effects
-RFASTA=/data/scratch/digenovaa/Somatic-reference-free/SNV-INDELs/programs/ensembl/Homo_sapiens.GRCh38.dna.primary_assembly.fa
-RGFF3=/data/scratch/digenovaa/Somatic-reference-free/SNV-INDELs/programs/ensembl/Homo_sapiens.GRCh38.99.chr.gff3.gz
+RFASTA=${ROOT_D}/databases/genome/Homo_sapiens.GRCh38.dna.primary_assembly.fa
+RGFF3=${ROOT_D}/databases/genome/Homo_sapiens.GRCh38.99.chr.gff3.gz
 
 add_effect=$(patsubst %.centro_cgenes.vcf.bgz,%.csq.vcf.bgz,$(add_centro_cgenes))
 %.csq.vcf.bgz: %.centro_cgenes.vcf.bgz
-	${BCFTOOLS} annotate --rename-chrs chr-names2.txt $< | ${BCFTOOLS} csq -p a -O z -o $@ -f ${RFASTA} -g ${RGFF3} -
+	${BCFTOOLS} annotate --rename-chrs ${ROOT_D}/databases/misc/chr-names2.txt $< | ${BCFTOOLS} csq -p a -O z -o $@ -f ${RFASTA} -g ${RGFF3} -
 add_csq:$(add_effect)
 
-#we mark the SOMATICS Vars and annot commo vars
+#we mark the SOMATICS Vars and annot common vars
 annot_somatics=$(patsubst %.csq.vcf.bgz,%.annot_somatics.vcf.bgz,$(add_effect))
 
 #Annot somatics
 %.annot_somatics.vcf.bgz:%.csq.vcf.bgz
-	bcftools annotate -a ../matched/$(subst .tonly.csq.vcf.bgz,.csq.vcf.bgz,$<) -m +SOMATIC -O z -o $@ $< 
+	${BCFTOOLS} annotate -a ../matched/$(subst .tonly.csq.vcf.bgz,.csq.vcf.bgz,$<) -m +SOMATIC -O z -o $@ $< 
 som:$(annot_somatics)
 
 #split somatics from germline
 split_sg=$(patsubst %.annot_somatics.vcf.bgz,%.somatics.vcf.bgz,$(annot_somatics))
 %.somatics.vcf.bgz:%.annot_somatics.vcf.bgz
-	bcftools  view -e "SOMATIC==1" -O z -o $(subst .annot_somatics.vcf.bgz,.germline.vcf.bgz,$<) $<
-	bcftools  view -i "SOMATIC==1" -O z -o $(subst .annot_somatics.vcf.bgz,.somatics.vcf.bgz,$<) $<
+	${BCFTOOLS}  view -e "SOMATIC==1" -O z -o $(subst .annot_somatics.vcf.bgz,.germline.vcf.bgz,$<) $<
+	${BCFTOOLS}  view -i "SOMATIC==1" -O z -o $(subst .annot_somatics.vcf.bgz,.somatics.vcf.bgz,$<) $<
 split:$(split_sg)
 # we built the variants matrix
 matrix=$(patsubst %.somatics.vcf.bgz,%.somatics.snv.matrix,$(split_sg))
 #CMF=/Users/adigenova/Projects/IARC/bioinfo/iarcbioinfo/TON-Calling/VCF/extract_feature_from_VCF.pl
-CMF=/home/digenovaa/scratch/Somatic-reference-free/SNV-INDELs/code/iarcbioinfo/TON-Calling/VCF/extract_feature_from_VCF.pl
+CMF=${ROOT_D}/code/perl/VCF/extract_feature_from_VCF.pl
 %.somatics.snv.matrix:%.somatics.vcf.bgz
 	perl ${CMF} -a $< > $@.log
 	perl ${CMF} -a $(subst .somatics.vcf.bgz,.germline.vcf.bgz,$<) > $@.germ.log
@@ -84,7 +74,6 @@ Somatics.snv.matrix.txt:$(matrix)
 	cat $(subst .somatics.snv.matrix,.germline.indel.matrix,$(matrix)) |  grep -v "FILE"| awk -v colums=${COLUMS} 'BEGIN{print colums}{a[$$2"-"$$3]++; b[$$2"-"$$3]=$$0}END{for(i in a){print b[i]" "a[i]" 0"}}' | sed 's/_filtered_PASS_norm.tonly.germline.vcf.bgz//g' > Germline.indel.matrix.txt
 
 #remove MESO_061 from data
-
 Somatics.snv.matrix.WMESO_061.txt:Somatics.snv.matrix.txt
 	grep -v "MESO_061" Somatics.snv.matrix.txt   > Somatics.snv.matrix.WMESO_061.txt
 	grep -v "MESO_061" Somatics.indel.matrix.txt > Somatics.indel.matrix.WMESO_061.txt
@@ -96,4 +85,4 @@ Somatics.snv.matrix.WMESO_061.txt:Somatics.snv.matrix.txt
 	egrep "^FILE|MESO_061" Germline.snv.matrix.txt   > Germline.snv.matrix.MESO_061.txt 
 	egrep "^FILE|MESO_061" Germline.indel.matrix.txt > Germline.indel.matrix.MESO_061.txt
 
-all: stats shared gnomad_annot comic_annot centro_genes add_csq som split to_matrix Somatics.snv.matrix.txt Somatics.snv.matrix.WMESO_061.txt
+all: gnomad_annot comic_annot centro_genes add_csq som split to_matrix Somatics.snv.matrix.txt Somatics.snv.matrix.WMESO_061.txt
---
+++ databases/genome/README.txt
@@ -0,0 +1,19 @@
+#ensembl
+
+version 99
+
+
+## genome
+
+
+```
+wget ftp://ftp.ensembl.org/pub/release-99/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+```
+
+the genome sequene file should be uncompressed.
+
+## Annotation
+
+```
+wget ftp://ftp.ensembl.org/pub/release-99/gff3/homo_sapiens/Homo_sapiens.GRCh38.99.chr.gff3.gz
+```
