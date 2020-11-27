01d77804ee6ba24aea8d83594378642a848f53d8
adigenova <digenova@gmail.com>
2020-11-27 12:12:59 +0100

    Filter VCFs and RF prob tag

--- code/perl/filter-vcf.pl
+++ code/perl/filter-vcf.pl
@@ -10,60 +10,90 @@ use Getopt::Std;
 use strict;
 
 sub usage {
-   print "$0 usage : -a  -b  -c\n";
+   print "$0 usage : -a <rf_model_snv_pred.txt> -b <rf_model_indel_pred.txt> -c <cutoff:0.5>  -d <vcf> -s <sample> \n";
    print "Error in use\n";
    exit 1;
 }
 
 my %opts = ();
-getopts( "a:b:c:", \%opts );
-if ( !defined $opts{a}  ) {
+getopts( "a:b:c:d:s:", \%opts );
+if ( !defined $opts{a} or !defined $opts{b} or !defined $opts{d} or !defined $opts{s} ) {
    usage;
 }
 
-my ($hash)=load_vars($opts{a},$opts{b});
+my $cuttof=0.5;
+if(defined $opts{c}){
+	$cuttof=$opts{c};
+}
+
+#we get the decision
+my ($hsnp)=load_vars($opts{a},$cuttof,$opts{s});
+my ($hindel)=load_vars($opts{b},$cuttof,$opts{s});
+
+
 #print Dumper($hash);
-open(FILE,"gzip -dc $opts{c}|") or die "cannot open file $opts{c}\n";
+print STDERR "Total somatic SNVs loaded ($opts{s})  :". scalar(keys %{$hsnp}) ."\n";
+print STDERR "Total somatic INDELs loaded ($opts{s}):". scalar(keys %{$hindel}) ."\n";
+#print Dumper($hash);
+open(FILE,"gzip -dc $opts{d}|") or die "cannot open file $opts{d}\n";
+
+my $somatic=0;
+
+my $bases={A=>1,C=>1,T=>1,G=>1};
 
 while(my $line=<FILE>){
 	chomp $line;
         my @data=split /\t/,$line;
 	if($line =~m/^#/){
-		print $line."\n";
-	}else{
-		if(defined $hash->{$data[0]}->{$data[1]}){
-			print $line."\n";
+		if($line =~m/#CHROM/){
+			#print "##FORMAT=<ID=RF_SP,Number=A,Type=Float,Description=\"Random Forest probability for somatic class\">\n";
+			#print $line."\n";
+		}else{
+		 	#print $line."\n";
 		}
+		
+	}else{
+		#we check if variants is SNP
+		if(defined $bases->{$data[3]} and defined $bases->{$data[4]}){
+			if(defined $hsnp->{join("__",$data[0],$data[1])}){
+				my $prob=$hsnp->{join("__",$data[0],$data[1])};
+				$data[7]="RF_SP=$prob;".$data[7];
+				#print join("\t",@data)."\n";
+				$somatic++;
+			}
+              }else{
+			#we try an indel 
+			if(defined $hindel->{join("__",$data[0],$data[1])}){
+				my $prob=$hindel->{join("__",$data[0],$data[1])};
+				$data[7]="RF_SP=$prob;".$data[7];
+				#print join("\t",@data)."\n";
+				$somatic++;
+			}
+	      }
 	}
 }
 
-
-
+#SOMATIC
+print STDERR "A total of $somatic variants were annotated for $opts{s}\n";
 
 
 
 
 sub load_vars{
-	my ($file,$sample)=@_;
+	my ($file,$cutoff, $sample)=@_;
 	open(FILE,$file) or die "cannot open file $file\n";
 	my $hash=();
 	while(my $line=<FILE>){
 	    	chomp $line;
         	my @data=split /\t/,$line;
-		next if ($data[1] ne $sample);
+		next if($data[1] ne $sample);
+		#print $data[$#data-4]."\n";
+		#print $data[$#data-3]."\n";
 		#next if not somatic
-		next if($data[$#data] != 1);
-		if($data[2] =~m/chr/){
-					
-		}else{
-			$data[2]="chr".$data[2];
+		if($data[$#data-3] > $cuttof ){
+			#print join("\t",@data)."\n";
+			$hash->{join("__",$data[2],$data[3])}=$data[$#data-3];
 		}
-		if($data[2] =~m/MT/){
-			#print STDERR $line."\n";
-			$data[2]="chrM";
-		}
-		#print join("\t",@data)."\n";
-		$hash->{$data[2]}->{$data[3]}=$data[4];
 	}
 	close(FILE);
 	return $hash;
