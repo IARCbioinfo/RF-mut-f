6aecf83a3d469baac941830c5886ba454976c1a3
adigenova <digenova@gmail.com>
2020-12-21 17:05:57 +0100

    adding makefile for filtering VCF and annovar with model(coding)

---
+++ code/makefiles/filter_vcfs_using_model_coding.mk
@@ -0,0 +1,24 @@
+.DELETE_ON_ERROR:
+	
+ROOT_D=/data/scratch/digenovaa/Somatic-reference-free/SNV-INDELs/RF-mut-f
+RF_SNV=${ROOT_D}/mesomics/release2/t-only/rf_snv_uncollapsed_m61.r1_predictions.txt
+RF_INDEL=${ROOT_D}/mesomics/release2/t-only/rf_indel_uncollapsed_m61.r1_predictions.txt
+
+#code that filter the VCFs or annovar files using the random forest
+PHOME=${ROOT_D}/code/perl
+
+filter_m61=$(patsubst %_filtered_PASS_norm.vcf.hg38_multianno.vcf.gz,%.pass_rf_filter.vcf.gz,$(wildcard *multianno.vcf.gz))
+%.pass_rf_filter.vcf.gz:%_filtered_PASS_norm.vcf.hg38_multianno.vcf.gz
+	perl ${PHOME}/filter-vcf.pl -a ${RF_SNV} -b ${RF_INDEL} -d $< -s $(subst _filtered_PASS_norm.vcf.hg38_multianno.vcf.gz,,$<) 2> $(subst .vcf.gz,.hc.vcf,$@).err
+	bgzip -i $(subst .gz,,$@)
+filter_meso61:$(filter_m61)
+
+
+#annovar files
+filter_an_m61=$(patsubst %_filtered_PASS_norm.vcf.hg38_multianno.txt,%.pass_rf_filter.hg38_multianno.txt,$(wildcard *vcf.hg38_multianno.txt))
+%.pass_rf_filter.hg38_multianno.txt:%_filtered_PASS_norm.vcf.hg38_multianno.txt
+	perl ${PHOME}/filter-multianno.pl -a ${RF_SNV} -b ${RF_INDEL} -d $< -s $(subst _filtered_PASS_norm.vcf.hg38_multianno.txt,,$<) 2>$@.err
+
+filter_an_meso61:$(filter_an_m61)
+
+all: filter_meso61 filter_an_meso61
