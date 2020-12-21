09f038823b9f6f9dc266e475d5eeac8a5866efcf
adigenova <digenova@gmail.com>
2020-12-21 17:54:26 +0100

    compressing unmapped vars

--- code/makefiles/filter_vcfs_using_model_coding.mk
+++ code/makefiles/filter_vcfs_using_model_coding.mk
@@ -1,5 +1,5 @@
 .DELETE_ON_ERROR:
-	
+
 ROOT_D=/data/scratch/digenovaa/Somatic-reference-free/SNV-INDELs/RF-mut-f
 RF_SNV=${ROOT_D}/mesomics/release2/t-only/rf_snv_uncollapsed_m61.r1_predictions.txt
 RF_INDEL=${ROOT_D}/mesomics/release2/t-only/rf_indel_uncollapsed_m61.r1_predictions.txt
@@ -9,8 +9,9 @@ PHOME=${ROOT_D}/code/perl
 
 filter_m61=$(patsubst %_filtered_PASS_norm.vcf.hg38_multianno.vcf.gz,%.pass_rf_filter.vcf.gz,$(wildcard *multianno.vcf.gz))
 %.pass_rf_filter.vcf.gz:%_filtered_PASS_norm.vcf.hg38_multianno.vcf.gz
-	perl ${PHOME}/filter-vcf.pl -a ${RF_SNV} -b ${RF_INDEL} -d $< -s $(subst _filtered_PASS_norm.vcf.hg38_multianno.vcf.gz,,$<) 2> $(subst .vcf.gz,.hc.vcf,$@).err
+	perl ${PHOME}/filter-vcf.pl -a ${RF_SNV} -b ${RF_INDEL} -d $< -s $(subst _filtered_PASS_norm.vcf.hg38_multianno.vcf.gz,,$<) 2> $(subst .vcf.gz,.vcf,$@).err
 	bgzip -i $(subst .gz,,$@)
+	bgzip -i $(subst _filtered_PASS_norm.vcf.hg38_multianno.vcf.gz,,$<).non_pass_rf_filter.vcf
 filter_meso61:$(filter_m61)
 
 
