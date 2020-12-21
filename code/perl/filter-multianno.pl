e53d6d9ab2c60def839254709d9bb0ab439d0d38
adigenova <digenova@gmail.com>
2020-12-21 16:39:07 +0100

    add CODING and RF_SP to annovar and vcf files

--- code/perl/filter-multianno.pl
+++ code/perl/filter-multianno.pl
@@ -1,5 +1,5 @@
 ###############################################################################
-# Author: Alex Di Genova 
+# Author: Alex Di Genova
 # Laboratory: IARC/SCG/RCG
 # Copyright (c)
 # year: 2020
@@ -21,7 +21,7 @@ if ( !defined $opts{a} or !defined $opts{b} or !defined $opts{d} or !defined $op
    usage;
 }
 
-my $cuttof=0.5;
+my $cuttof=0;
 if(defined $opts{c}){
 	$cuttof=$opts{c};
 }
@@ -36,56 +36,130 @@ print STDERR "Total somatic SNVs loaded ($opts{s})  :". scalar(keys %{$hsnp}) ."
 print STDERR "Total somatic INDELs loaded ($opts{s}):". scalar(keys %{$hindel}) ."\n";
 #print Dumper($hash);
 open(FILE, $opts{d}) or die "cannot open file $opts{d}\n";
+open(NPVCF,">".$opts{s}.".non_pass_rf_filter.hg38_multianno.txt") or die "cannot open file $opts{s}\n";
+open(PFVCF,">".$opts{s}.".pass_rf_filter.hg38_multianno.txt") or die "cannot open file $opts{s}\n";
+
 
 my $somatic=0;
 my $bases={A=>1,C=>1,T=>1,G=>1};
 my $total_lines=0;
+my $total=0;
+#coding is exonic + splicing
+my $reg_types={"intergenic"=>0,"intronic"=>0,"exonic"=>1,"downstream"=>0,"upstream"=>0,
+               "splicing"=>1,"UTR3"=>0,"UTR5"=>0,"ncRNA_UTR5"=>0,
+               "ncRNA_exonic"=>0,"ncRNA_intronic"=>0,"ncRNA_splicing"=>0};
+
+
+               my $coding=0;
+               my $non_coding=0;
 while(my $line=<FILE>){
         chomp $line;
 	$total_lines++;
         my @data=split /\t/,$line;
 	if($line=~m/^Chr/){
-		print $line."\n";
+    my @h=split("\t",$line);
+    #we add the two values in the array
+    #splice(@h,@h,4,"RF_SP","IS_CODING");
+		print NPVCF join("\t",@h[0 .. 4],"RF_SP","IS_CODING",@h[5 .. $#h])."\n";
+    print PFVCF join("\t",@h[0 .. 4],"RF_SP","IS_CODING",@h[5 .. $#h])."\n";
 	}else{
+    $total++;
 		#we check if variants is SNP
+    my ($is_coding)=is_coding($data[5],$reg_types);
+    #we try a SNV variant
 		if(defined $bases->{$data[3]} and defined $bases->{$data[4]}){
 			if(defined $hsnp->{join("__",$data[0],$data[1])}){
-				#print $line." $somatic\n";
-				print $line."\n";
-				$somatic++;
+			  my $prob=$hsnp->{join("__",$data[0],$data[1])};
+        if($is_coding and $prob > 0.5){
+          #splice(@data,@data,4,$prob,$is_coding);
+          print PFVCF join("\t",@data[0 .. 4],$prob,$is_coding,@data[5 .. $#data] )."\n";
+          $coding++;
+          $somatic++;
+        }elsif($prob > 0.75){
+          print PFVCF join("\t",@data[0 .. 4],$prob,$is_coding,@data[5 .. $#data] )."\n";
+          $non_coding++;
+          $somatic++;
+        }else{
+          print NPVCF join("\t",@data[0 .. 4],$prob,$is_coding,@data[5 .. $#data] )."\n";
+        }
 			}else{
 				#print STDERR "NOT FOUND\n".$line."\n";
+        #variant not classified 4 in MESO_084_T
+         print NPVCF join("\t",@data[0 .. 4],"NA","NA",@data[5 .. $#data] )."\n";
 			}
-              }else{
-			#we try an indel 
-			if(defined $hindel->{join("__",$data[0],$data[1])}){
-				print $line."\n";
-				$somatic++;
-			#annovar encode alt deletions increasing pos+1, example below:
+     }else{
+			#we try an indel
+      #annovar encode alt deletions increasing pos+1, example below:
 			#chr1    941119  941119  A       -       intronic
-			#MESO_084_T      chr1    941118  NN      MODIFIER#	
+			#MESO_084_T      chr1    941118  NN      MODIFIER#
 			#for ref deletions is not the case
-			#chr1    3086979 3086979 -       A	
-			#MESO_084_T      chr1    3086979 NN      MODIFIER	
-			#INDEL case
+			#chr1    3086979 3086979 -       A
+			#MESO_084_T      chr1    3086979 NN      MODIFIER
+
+			if(defined $hindel->{join("__",$data[0],$data[1])}){
+				#print $line."\n";
+        my $prob=$hindel->{join("__",$data[0],$data[1])};
+        if($is_coding and $prob > 0.5){
+          #splice(@data,@data,4,$prob,$is_coding);
+          print PFVCF join("\t",@data[0 .. 4],$prob,$is_coding,@data[5 .. $#data] )."\n";
+          $coding++;
+          $somatic++;
+        }elsif($prob > 0.75){
+          print PFVCF join("\t",@data[0 .. 4],$prob,$is_coding,@data[5 .. $#data] )."\n";
+          $non_coding++;
+          $somatic++;
+        }else{
+          print NPVCF join("\t",@data[0 .. 4],$prob,$is_coding,@data[5 .. $#data] )."\n";
+        }
+				#$somatic++;
+      	#INDEL case
 			}elsif(defined $hindel->{join("__",$data[0],$data[1]-1)} and length($data[3]) > length($data[4])){
 				#print $line." $somatic\n";
-				print $line."\n";
-				$somatic++;
+        my $prob=$hindel->{join("__",$data[0],$data[1]-1)};
+				#print $line."\n";
+				#$somatic++;
+        if($is_coding and $prob > 0.5){
+          #splice(@data,@data,4,$prob,$is_coding);
+          print PFVCF join("\t",@data[0 .. 4],$prob,$is_coding,@data[5 .. $#data] )."\n";
+          $coding++;
+          $somatic++;
+        }elsif($prob > 0.75){
+          print PFVCF join("\t",@data[0 .. 4],$prob,$is_coding,@data[5 .. $#data] )."\n";
+          $non_coding++;
+          $somatic++;
+        }else{
+          print NPVCF join("\t",@data[0 .. 4],$prob,$is_coding,@data[5 .. $#data] )."\n";
+        }
+
 			# handle the MNP case, such as CC>TT
 			}elsif(length($data[3]) == length($data[4]) and $data[4] eq "-" and defined $hindel->{join("__",$data[0],$data[1]-1)}){
 				#print STDERR "NOT FOUND\n".$line."\n";
-				print $line."\n";
-				$somatic++;
+				#print $line."\n";
+        my $prob=$hindel->{join("__",$data[0],$data[1]-1)};
+        if($is_coding and $prob > 0.5){
+          #splice(@data,@data,4,$prob,$is_coding);
+          print PFVCF join("\t",@data[0 .. 4],$prob,$is_coding,@data[5 .. $#data] )."\n";
+          $coding++;
+          $somatic++;
+        }elsif($prob > 0.75){
+          print PFVCF join("\t",@data[0 .. 4],$prob,$is_coding,@data[5 .. $#data] )."\n";
+          $non_coding++;
+          $somatic++;
+        }else{
+          print NPVCF join("\t",@data[0 .. 4],$prob,$is_coding,@data[5 .. $#data] )."\n";
+        }
+				#$somatic++;
 			}
-	      }
-	
+	   }
+
 	}
 }
 
 
 #SOMATIC
-print STDERR "A total of $somatic variants were annotated for $opts{s}\n";
+#print STDERR "A total of $somatic variants were annotated for $opts{s}\n";
+print STDERR "A total of $somatic [coding=$coding, non_coding=$non_coding ] variants were classified as somatic for $opts{s} (Total=$total)\n";
+
 #print STDERR "Total lines  $total_lines in $opts{s}\n";
 
 sub load_vars{
@@ -97,12 +171,23 @@ sub load_vars{
         	my @data=split /\t/,$line;
 		next if($data[1] ne $sample);
 		#next if not somatic
-		if($data[$#data-3] > $cuttof ){
+		#if($data[$#data-3] > $cuttof ){
 			$hash->{join("__",$data[2],$data[3])}=$data[$#data-3];
-		}
+		#}
 	}
 	close(FILE);
 	return $hash;
 }
 
-
+#ask if var is coding using the types
+sub is_coding{
+	my ($line,$types)=@_;
+	my @tags=split(";",$line);
+	if(defined $types->{$tags[0]}){
+			return  $types->{$tags[0]};
+	}else{
+		print STDERR $tags[0]." not found in types\n";
+		print STDERR Dumper($tags[0]);
+		print STDERR Dumper($types);
+	}
+}
--- code/perl/filter-vcf.pl
+++ code/perl/filter-vcf.pl
@@ -21,7 +21,7 @@ if ( !defined $opts{a} or !defined $opts{b} or !defined $opts{d} or !defined $op
    usage;
 }
 
-my $cuttof=0.5;
+my $cuttof=0;
 if(defined $opts{c}){
 	$cuttof=$opts{c};
 }
@@ -36,47 +36,121 @@ print STDERR "Total somatic SNVs loaded ($opts{s})  :". scalar(keys %{$hsnp}) ."
 print STDERR "Total somatic INDELs loaded ($opts{s}):". scalar(keys %{$hindel}) ."\n";
 #print Dumper($hash);
 open(FILE,"gzip -dc $opts{d}|") or die "cannot open file $opts{d}\n";
+open(NPVCF,">".$opts{s}.".non_pass_rf_filter.vcf") or die "cannot open file $opts{s}\n";
+open(PFVCF,">".$opts{s}.".pass_rf_filter.vcf") or die "cannot open file $opts{s}\n";
 
 my $somatic=0;
-
+my $total=0;
 my $bases={A=>1,C=>1,T=>1,G=>1};
+my $is_cod=();
+my $coding=0;
+my $non_coding=0;
+#coding is exonic + splicing
+my $reg_types={"intergenic"=>0,"intronic"=>0,"exonic"=>1,"downstream"=>0,"upstream"=>0,
+               "splicing"=>1,"UTR3"=>0,"UTR5"=>0,"ncRNA_UTR5"=>0,
+               "ncRNA_exonic"=>0,"ncRNA_intronic"=>0,"ncRNA_splicing"=>0};
+
 
 while(my $line=<FILE>){
 	chomp $line;
         my @data=split /\t/,$line;
 	if($line =~m/^#/){
 		if($line =~m/#CHROM/){
-			print "##FORMAT=<ID=RF_SP,Number=A,Type=Float,Description=\"Random Forest probability for somatic class\">\n";
-			print $line."\n";
+			print PFVCF "##FORMAT=<ID=RF_SP,Number=A,Type=Float,Description=\"Random Forest probability for somatic class\">\n";
+			print PFVCF "##FORMAT=<ID=IS_CODING,Number=A,Type=Integer,Description=\"Indicate if the variant is coding or not (Annovar classification)\">\n";
+			print PFVCF $line."\n";
+
+			print NPVCF "##FORMAT=<ID=RF_SP,Number=A,Type=Float,Description=\"Random Forest probability for somatic class\">\n";
+			print NPVCF "##FORMAT=<ID=IS_CODING,Number=A,Type=Integer,Description=\"Indicate if the variant is coding or not (Annovar classification)\">\n";
+			print NPVCF $line."\n";
 		}else{
-		 	print $line."\n";
+		 	print PFVCF $line."\n";
+		 	print NPVCF $line."\n";
 		}
 		
 	}else{
+		$total++;
+		#we check if the variant is coding or not
+		my ($is_coding)=is_coding($data[7],$reg_types);
+		#$is_cod->{$is_coding}++;
 		#we check if variants is SNP
 		if(defined $bases->{$data[3]} and defined $bases->{$data[4]}){
 			if(defined $hsnp->{join("__",$data[0],$data[1])}){
 				my $prob=$hsnp->{join("__",$data[0],$data[1])};
-				$data[7]="RF_SP=$prob;".$data[7];
-				print join("\t",@data)."\n";
-				$somatic++;
+				$data[7]="IS_CODING=$is_coding;RF_SP=$prob;".$data[7];
+				if($is_coding and $prob > 0.5){
+					print PFVCF join("\t",@data)."\n";
+					$coding++;
+					$somatic++;
+				}elsif($prob > 0.75){
+				 	print PFVCF join("\t",@data)."\n";
+					$non_coding++;
+					$somatic++;
+				}else{
+				 	print NPVCF  join("\t",@data)."\n";
+				}	
+				
+			}else{
+				 print NPVCF  join("\t",@data)."\n";
 			}
               }else{
 			#we try an indel 
 			if(defined $hindel->{join("__",$data[0],$data[1])}){
 				my $prob=$hindel->{join("__",$data[0],$data[1])};
-				$data[7]="RF_SP=$prob;".$data[7];
-				print join("\t",@data)."\n";
-				$somatic++;
+				$data[7]="IS_CODING=$is_coding;RF_SP=$prob;".$data[7];
+				if($is_coding and $prob > 0.5){
+					print PFVCF join("\t",@data)."\n";
+					$coding++;
+					$somatic++;
+				}elsif($prob > 0.75){
+				 	print PFVCF join("\t",@data)."\n";
+					$non_coding++;
+					$somatic++;
+				}else{
+				 	print NPVCF join("\t",@data)."\n";
+				}
+				#print join("\t",@data)."\n";
+			}else{
+				#var not classified
+				 print NPVCF join("\t",@data)."\n";
 			}
 	      }
 	}
 }
 
 #SOMATIC
-print STDERR "A total of $somatic variants were annotated for $opts{s}\n";
+print STDERR "A total of $somatic [coding=$coding, non_coding=$non_coding ] variants were classified as somatic for $opts{s} (Total=$total)\n";
+#print Dumper($is_cod);
 
-
+sub is_coding{
+	my ($line,$types)=@_;
+	my @tags=split(";",$line);
+	my $hash=();
+	foreach my $tt(@tags){
+		my ($t,$v)=split("=",$tt);
+		#next if($v eq ".");
+		if($v=~m/x3/){
+			my $p=index($v,"x3");
+			#print STDERR $v." match"." at pos ".$p." sub ".substr($v,0,$p-1)."\n";
+			my $a=substr($v,0,$p-1);
+			$v=$a;
+		}
+		$hash->{$t}=$v;
+	}	
+	#print Dumper($hash);
+	#print Dumper($hash->{"Func.ensGene"});
+	#print $hash->{"Func.ensGene"}."\n";
+	if(defined $types->{$hash->{"Func.ensGene"}}){
+			return  $types->{$hash->{"Func.ensGene"}};
+	}else{
+		print STDERR $hash->{"Func.ensGene"}." not found in types\n";
+		print STDERR Dumper($hash->{"Func.ensGene"});
+		print STDERR Dumper($types);
+	}
+	#return $hash->{"Func.ensGene"};
+		
+	#return $hash->{"ExonicFunc.ensGene"};
+}
 
 
 sub load_vars{
@@ -90,10 +164,10 @@ sub load_vars{
 		#print $data[$#data-4]."\n";
 		#print $data[$#data-3]."\n";
 		#next if not somatic
-		if($data[$#data-3] > $cuttof ){
+		#if($data[$#data-3] > $cuttof ){
 			#print join("\t",@data)."\n";
 			$hash->{join("__",$data[2],$data[3])}=$data[$#data-3];
-		}
+		#}
 	}
 	close(FILE);
 	return $hash;
